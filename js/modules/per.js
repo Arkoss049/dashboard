44baf626c36b572f284a3d90f59c9c95c212f07d
Arkoss049 <arkoss049@gmail.com>
2025-08-27 13:23:45 +0200

    Create per.js

---
+++ js/modules/per.js
@@ -0,0 +1,7 @@
+// Contenu pour js/modules/per.js
+
+function initPerPanel() {
+    // Ce module n'a pas besoin de logique, 
+    // l'iframe gère le simulateur de manière autonome.
+    console.log("Module Simulateur PER chargé via iframe.");
+}
