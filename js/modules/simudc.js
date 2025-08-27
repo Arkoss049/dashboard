7c857c8298c87fd700507301d26aad1751a8ce2c
Arkoss049 <arkoss049@gmail.com>
2025-08-27 15:01:06 +0200

    Create simudc.js

---
+++ js/modules/simudc.js
@@ -0,0 +1,7 @@
+// Contenu pour js/modules/simudc.js
+
+function initSimudcPanel() {
+    // Ce module n'a pas besoin de logique, 
+    // l'iframe gère le simulateur de manière autonome.
+    console.log("Module Simulateur DC chargé via iframe.");
+}
